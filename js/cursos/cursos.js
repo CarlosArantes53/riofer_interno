d56c372696b6fedbdb61c93325b9a7f367f3fecb
Carlos-Riofer <carlos.eduardo@riofer.com.br>
2024-08-20 13:39:12 -0300

    Login

--- js/cursos/cursos.js
+++ js/cursos/cursos.js
@@ -24,6 +24,6 @@ auth.onAuthStateChanged((user) => {
   if (user) {
     listarCursos();
   } else {
-    window.location.href = '../html/home.html';
+    window.location.href = '../html/login.html';
   }
 });
