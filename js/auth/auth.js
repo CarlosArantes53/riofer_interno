6b1172f9bea27e97c3a19b6f24a34c787bd9424d
arthur.lima <arthur.lima@riofer.com.br>
2024-09-04 18:06:39 -0300

    04/09

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -1,13 +1,26 @@
+// auth.js
+
+// Adicione o listener ao formulário de login
 document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+    e.preventDefault(); // Previne o comportamento padrão do formulário
+
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     auth.signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            redirectToHome();
+            // Login bem-sucedido
+            const user = userCredential.user;
+            console.log('Usuário logado:', user.email);
+            redirectToHome(); // Função para redirecionar para a página inicial
         })
         .catch((error) => {
+            // Exibe a mensagem de erro no elemento
             document.getElementById('error-message').textContent = error.message;
         });
 });
+
+// Função para redirecionar para a página inicial
+function redirectToHome() {
+    window.location.href = '/paginas/home/home.html';
+}
